600d9edde8f99bd939b57e2d5295411b21250cd7
Ariane Saulnier <asaulnie@made-f0Ar3s12.clusters.42paris.fr>
2024-11-23 17:15:07 +0100

    added union (checked) and paramsum (checked)

---
+++ lvl3/paramsum/paramsum.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaulnie <asaulnie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 16:51:57 by asaulnie          #+#    #+#             */
+/*   Updated: 2024/11/23 17:13:31 by asaulnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(int n)
+{
+	char	digit;
+
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	digit = (n % 10) + '0';
+	write(1, &digit, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argv)
+		ft_putnbr(argc - 1);
+	write(1, "\n", 1);
+	return (0);
+}
