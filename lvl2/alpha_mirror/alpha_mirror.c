e58d47ed8c9a0f13ed238ef0bd370a7e096f3718
Ariane Saulnier <asaulnie@made-f0Ar4s12.clusters.42paris.fr>
2024-11-24 16:42:48 +0100

    added alpha_mirror (checked)

--- lvl2/alpha_mirror/alpha_mirror.c
+++ lvl2/alpha_mirror/alpha_mirror.c
@@ -5,40 +5,50 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: asaulnie <asaulnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/08 19:12:58 by asaulnie          #+#    #+#             */
-/*   Updated: 2024/11/08 19:23:03 by asaulnie         ###   ########.fr       */
+/*   Created: 2024/11/24 15:46:38 by asaulnie          #+#    #+#             */
+/*   Updated: 2024/11/24 16:23:47 by asaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-// a = 97 z = 122
-// 0 -> 27
-// 1 -> 26
+// a = 97
+// z = 122
 
 void	alpha_mirror(char *str)
 {
 	int		i;
-	int		j;
-	char	*alpha;
-	int		end;
-	char	c;
+	int		l;
+	char	*alpha = "abcdefghijklmnopqrstuvwxyz";
+	char	*rev_alpha = "zyxwvutsrqponmlkjihgfedcba";
+	char	*alphaC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char	*rev_alphaC = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
 
 	i = 0;
-	j = 0;
-	alpha = "abcdefghijklmnopqrstuvwxyz";
-	end = 26;
+	l = 0;
 	while (str[i])
 	{
-		while (alpha[j])
+		l = 0;
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (str[i] == alpha[j])
+			while (alpha[l])
 			{
-				c = alpha[end - i];
-				write(1, &c, 1);
+				if (str[i] == alpha[l])
+					write(1, &rev_alpha[l], 1);
+				l++;
 			}
-			j++;
 		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			while (alphaC[l])
+			{
+				if (str[i] == alphaC[l])
+					write(1, &rev_alphaC[l], 1);
+				l++;
+			}
+		}
+		else
+			write(1, &str[i], 1);
 		i++;
 	}
 }
@@ -48,4 +58,5 @@ int	main(int argc, char **argv)
 	if (argc == 2)
 		alpha_mirror(argv[1]);
 	write(1, "\n", 1);
+	return (0);
 }
