fac9cef30528db856a113c1489c1a48a9cc2bbda
Ariane Saulnier <asaulnie@made-f0Ar9s8.clusters.42paris.fr>
2024-11-25 15:50:29 +0100

    added snake_to_camel (checked)

---
+++ lvl2/snake_to_camel/snake_to_camel.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   snake_to_camel.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaulnie <asaulnie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 15:44:10 by asaulnie          #+#    #+#             */
+/*   Updated: 2024/11/25 15:48:56 by asaulnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	snake_to_camel(char *str)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '_')
+		{
+			i++;
+			c = str[i] - 32;
+			write(1, &c, 1);
+		}
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		snake_to_camel(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
