98c421c5cd2b0119bccc220056524db3823d8f04
Ariane Saulnier <asaulnie@made-f0Br7s9.clusters.42paris.fr>
2024-11-26 11:00:07 +0100

    mod ft_strcpy(checked)

--- lvl1/ft_strcpy/ft_strcpy.c
+++ lvl1/ft_strcpy/ft_strcpy.c
@@ -6,12 +6,13 @@
 /*   By: asaulnie <asaulnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 14:19:55 by asaulnie          #+#    #+#             */
-/*   Updated: 2024/11/23 19:47:27 by asaulnie         ###   ########.fr       */
+/*   Updated: 2024/11/26 10:59:25 by asaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 char	*ft_strcpy(char *s1, char *s2)
 {
@@ -31,16 +32,10 @@ int	main(void)
 {
 	char	dest[10];
 	char	*src;
-	int		i;
 
-	src = "hi";
-	i = 0;
-	ft_strcpy(dest, src);
-	while (dest[i])
-	{
-		write(1, &dest[i], 1);
-		i++;
-	}
+	src = "test";
+	printf("ft: %s\n", ft_strcpy(dest, src));
+	printf("real: %s\n", strcpy(dest, src));
 	return (0);
 }
 */
