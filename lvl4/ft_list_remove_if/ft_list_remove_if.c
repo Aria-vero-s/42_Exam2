3861f64756f186cba2359dab1b3d6ab0711bd3d9
Ariane Saulnier <asaulnie@made-f0Ar3s7.clusters.42paris.fr>
2024-11-30 22:26:58 +0100

    mod ft_list_remove_if(not checked)

--- lvl4/ft_list_remove_if/ft_list_remove_if.c
+++ lvl4/ft_list_remove_if/ft_list_remove_if.c
@@ -1,47 +1,81 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_list_remove_if.c                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: asaulnie <asaulnie@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/08 16:46:30 by asaulnie          #+#    #+#             */
-/*   Updated: 2024/11/08 16:46:50 by asaulnie         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdlib.h>
+#include <stdio.h>
 
-#include <unistd.h>
-
-typedef struct	s_list
+typedef struct s_list
 {
 	struct s_list	*next;
-	void		*data;
-}			 t_list;
+	void			*data;
+}	t_list;
 
-void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list *curr = *begin_list;
-	t_list *to_remove;
+	t_list	*current;
+	t_list	*previous;
+	t_list	*to_free;
 
-	if(!(*begin_list))
-		return;
-
-	while(curr && cmp(curr->data, data_ref)== 0)
+	current = *begin_list;
+	previous = NULL;
+	if (current == NULL)
+		return ;
+	while (current != NULL)
 	{
-		to_remove = curr;
-		curr = curr->next;
-		*begin_list = curr;
-		free(to_remove);
-	}
-	while(curr)
-	{
-		if (curr->next && cmp(curr->next->data, data_ref)== 0)
+		if (cmp(current->data, data_ref) == 0)
 		{
-			to_remove = curr->next;
-			curr->next = curr->next->next;
-			free(to_remove);
+			if (previous == NULL)
+				*begin_list = current->next;
+			else
+				previous->next = current->next;
+			to_free = current;
+			current = current->next;
+			free(to_free);
 		}
 		else
-			curr = curr->next;
+		{
+			previous = current;
+			current = current->next;
+		}
 	}
+}
+
+int	cmp(void *data1, void *data2)
+{
+	if (data1 == data2)
+		return (0);
+	return (1);
+}
+
+int	main(void)
+{
+	t_list	*ptr;
+	t_list	*ptr2;
+	t_list	*ptr3;
+	t_list	*current;
+	t_list	**begin_list;
+
+	ptr = malloc(sizeof(t_list));
+	ptr2 = malloc(sizeof(t_list));
+	ptr3 = malloc(sizeof(t_list));
+
+	ptr->data = (void *)"test1";
+	ptr->next = ptr2;
+	ptr2->data = (void *)"test2";
+	ptr2->next = ptr3;
+	ptr3->data = (void *)"test3";
+	ptr3->next = NULL;
+
+	current = ptr;
+	while (current != NULL)
+	{
+		printf("Data avant : %s\n", (char *)current->data);
+		current = current->next;
+	}
+	begin_list = &ptr;
+	ft_list_remove_if(begin_list, "test1", cmp);
+	current = *begin_list;
+	while (current != NULL)
+	{
+		printf("Data apres: %s\n", (char *)current->data);
+		current = current->next;
+	}
+	return (0);
 }
