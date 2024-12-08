240f157b6f1cd6d751a7c900ce24130b35cf4f41
Ariane Saulnier <asaulnie@bess-f2r4s8.clusters.42paris.fr>
2024-12-08 13:10:58 +0100

    simplified lcm (checked)

--- lvl3/lcm/lcm.c
+++ lvl3/lcm/lcm.c
@@ -6,39 +6,54 @@
 /*   By: asaulnie <asaulnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 15:52:35 by asaulnie          #+#    #+#             */
-/*   Updated: 2024/11/25 17:27:30 by asaulnie         ###   ########.fr       */
+/*   Updated: 2024/12/08 13:09:50 by asaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <limits.h>
+
+// lcm (lowest common multiple)
+
+// if a = 2
+// 1*2, 2*2, 3*2, 4*2 etc
+// 2, 4, 6, 8, 10, etc are multiples of 2
+
+// if a = 6
+// 1*6, 2*6, 3*6, 4*6 etc
+// 6, 12, 18, 24, etc are multiples of 6
+
+// lcm = 6
 
 unsigned int	lcm(unsigned int a, unsigned int b)
 {
-	unsigned int	n;
+	unsigned int	mult_a;
+	unsigned int	mult_b;
 
+	mult_a = a;
+	mult_b = b;
 	if (a == 0 || b == 0)
 		return (0);
-	if (a > b)
-		n = a;
-	else
-		n = b;
-	while (1)
+	else if (a == 1)
+		return (b);
+	else if (b == 1)
+		return (a);
+	else if (a > INT_MAX || b > INT_MAX)
+		return (0);
+	while (mult_a != mult_b)
 	{
-		if (n % a == 0 && n % b == 0)
-			return (n);
-		n++;
+		if (mult_a < mult_b)
+			mult_a = mult_a + a;
+		else
+			mult_b = mult_b + b;
 	}
+	return (mult_a);
 }
 /*
-int	main(void)
+int	main()
 {
-	unsigned int	n1;
-	unsigned int	n2;
-
-	n1 = -345;
-	n2 = 78;
-	printf("lcm: %u\n", lcm(n1, n2));
+	printf("%d\n", lcm(3, -1));
 	return (0);
 }
 */
