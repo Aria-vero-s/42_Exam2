5157030e3bdbdc0073e335e2300c0ffda9765120
Ariane Saulnier <asaulnie@bess-f2r5s10.clusters.42paris.fr>
2024-12-10 12:16:06 +0100

    simplified do_op

--- lvl2/do_op/do_op.c
+++ lvl2/do_op/do_op.c
@@ -6,37 +6,35 @@
 /*   By: asaulnie <asaulnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 15:16:55 by asaulnie          #+#    #+#             */
-/*   Updated: 2024/11/21 13:16:06 by asaulnie         ###   ########.fr       */
+/*   Updated: 2024/12/10 12:15:49 by asaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+void	do_op(int a, char op, int b)
+{
+	int	result;
+
+	if (op == '+')
+		result = a + b;
+	else if (op == '-')
+		result = a - b;
+	else if (op == '*')
+		result = a * b;
+	else if (op == '/')
+		result = a / b;
+	else if (op == '%')
+		result = a % b;
+	printf("%d\n", result);
+}
 
 int	main(int argc, char **argv)
 {
-	int	num1;
-	int	num2;
-	int	result;
-
 	if (argc == 4)
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
-		if (argv[2][0] == '+')
-			result = num1 + num2;
-		else if (argv[2][0] == '-')
-			result = num1 - num2;
-		else if (argv[2][0] == '*')
-			result = num1 * num2;
-		else if (argv[2][0] == '/')
-			result = num1 / num2;
-		else if (argv[2][0] == '%')
-			result = num1 % num2;
-		printf("%d\n", result);
-	}
+		do_op(atoi(argv[1]), argv[2][0], atoi(argv[3]));
 	else
 		write(1, "\n", 1);
-	return (0);
 }
