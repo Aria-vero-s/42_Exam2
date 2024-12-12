e65061d730626112077abd4cd7cc326192a602ba
Ariane Saulnier <asaulnie@bess-f2r6s5.clusters.42paris.fr>
2024-12-12 14:55:13 +0100

    simplified rot_13 (checked)

--- lvl1/rot_13/rot_13.c
+++ lvl1/rot_13/rot_13.c
@@ -6,37 +6,41 @@
 /*   By: asaulnie <asaulnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 15:58:51 by asaulnie          #+#    #+#             */
-/*   Updated: 2024/10/07 18:38:18 by asaulnie         ###   ########.fr       */
+/*   Updated: 2024/12/12 14:54:57 by asaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+void	rot_13(char *str)
 {
 	int		i;
 	char	c;
 
-	if (argc == 2)
+	i = 0;
+	while (str[i])
 	{
-		i = 0;
-		while (argv[1][i] != '\0')
+		if ((str[i] >= 'a' && str[i] <= 'm')
+			|| (str[i] >= 'A' && str[i] <= 'M'))
 		{
-			if (('A' <= argv[1][i] && argv[1][i] <= 'Z') ||
-			('a' <= argv[1][i] && argv[1][i] <= 'z'))
-			{
-				if ((argv[1][i] >= 'a' && argv[1][i] <= 'm') ||
-				(argv[1][i] >= 'A' && argv[1][i] <= 'M'))
-					c = argv[1][i] + 13;
-				else
-					c = argv[1][i] - 13;
-				write(1, &c, 1);
-			}
-			else
-				write(1, &argv[1][i], 1);
-			i++;
+			c = str[i] + 13;
+			write(1, &c, 1);
 		}
+		else if ((str[i] >= 'n' && str[i] <= 'z')
+			|| (str[i] >= 'N' && str[i] <= 'Z'))
+		{
+			c = str[i] - 13;
+			write(1, &c, 1);
+		}
+		else
+			write(1, &str[i], 1);
+		i++;
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		rot_13(argv[1]);
 	write(1, "\n", 1);
-	return (0);
 }
